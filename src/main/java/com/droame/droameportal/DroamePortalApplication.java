759c1c0531c1b859c7b93506e5c5ca32bd705f41
Jitesh Singh <Jiteshs101@gmail.com>
2023-03-27 14:55:10 +0530

    Initial Structure

---
+++ src/main/java/com/droame/droameportal/DroamePortalApplication.java
@@ -0,0 +1,13 @@
+package com.droame.droameportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DroamePortalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DroamePortalApplication.class, args);
+    }
+
+}
